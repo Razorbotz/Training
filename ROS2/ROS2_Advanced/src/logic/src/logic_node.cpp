9bc9960dceb23ad7d71e57a0fde36f9b5b001572
andrewburroughs <andrewburroughs17@gmail.com>
2023-10-06 20:32:37 -0500

    Added PDFs and updated logic node

--- ROS2/ROS2_Advanced/src/logic/src/logic_node.cpp
+++ ROS2/ROS2_Advanced/src/logic/src/logic_node.cpp
@@ -58,21 +58,9 @@ float joystick1Throttle=0;
 float maxSpeed=0.4;
 float maxNeoSpeed = 0.1;
 
-bool automationGo=false;
-bool excavationGo = false;
-bool invertBucket = false;
-bool runSensorlessly = false;
-
-Automation* automation=new Automation1();
-
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > driveLeftSpeedPublisher;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > driveRightSpeedPublisher;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > shoulderPublisher;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > dumpPublisher;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > neoPublisher;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Bool_<std::allocator<void> >, std::allocator<void> > > automationGoPublisher;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > dumpBinPublisher;
-std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > stepperPublisher;
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > talonSpeedPublisher;
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > falconSpeedPublisher;
+std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Float32_<std::allocator<void> >, std::allocator<void> > > neoSpeedPublisher;
 
 
 /** @brief Function to initialize the motors to zero
@@ -87,12 +75,9 @@ void initSetSpeed(){
     std_msgs::msg::Float32 speed;
     speed.data = 0.0;
     
-    driveLeftSpeedPublisher->publish(speed);
-    driveRightSpeedPublisher->publish(speed);
-    shoulderPublisher->publish(speed);
-    dumpPublisher->publish(speed);
-    neoPublisher->publish(speed);
-    //RCLCPP_INFO(nodeHandle->get_logger(), "Set init motor speeds to 0.0");
+    talonSpeedPublisher->publish(speed);
+    falconSpeedPublisher->publish(speed);
+    neoSpeedPublisher->publish(speed);
 }
 
 /** @brief Function to update speed of the wheels
@@ -105,21 +90,10 @@ void initSetSpeed(){
  * */
 void updateSpeed(){
     
-    std_msgs::msg::Float32 speedLeft;
-    std_msgs::msg::Float32 speedRight;
-    
-    //Linear transformation of cordinate planes
-    speedLeft.data  = (joystick1Pitch + joystick1Roll);
-    speedRight.data = (joystick1Pitch - joystick1Roll);
-    
-    //multiplied the output speed by the "throttle" value and a limiting precentage
-    speedLeft.data  = speedLeft.data * maxSpeed;
-    speedRight.data = speedRight.data * maxSpeed;
-    
-    RCLCPP_INFO(nodeHandle->get_logger(),"speed left=%f right=%f  pitch=%f roll=%f", speedLeft.data,  speedRight.data, joystick1Pitch, joystick1Roll);
-
-    driveLeftSpeedPublisher->publish(speedLeft);
-    driveRightSpeedPublisher->publish(speedRight);
+    std_msgs::msg::Float32 speed;
+    speed.data  = (joystick1Pitch + joystick1Roll);
+    speed.data  = speed.data * maxSpeed;
+    falconSpeedPublisher->publish(speed);
 }
 
 /** @brief Function to stop drive train motors
@@ -133,46 +107,9 @@ void updateSpeed(){
 void stopSpeed(){
     std_msgs::msg::Float32 speed;
     speed.data = 0.0;
-    driveLeftSpeedPublisher->publish(speed);
-    driveRightSpeedPublisher->publish(speed);
-}
-
-/** @brief Function to update excavation motor speeds
- * 
- * This function is called when the node receives 
- * joystick information and is in the excavation 
- * state.  The function publishes the shoulder speed,
- * the arm speed, and the drum speeed.
- * @return void
- * */
-void updateExcavation(){
-    std_msgs::msg::Float32 speed;
-    speed.data = joystick1Pitch;
-    shoulderPublisher->publish(speed);
-    std_msgs::msg::Float32 throttleSpeed;
-    throttleSpeed.data = joystick1Throttle * maxNeoSpeed;
-    neoPublisher->publish(throttleSpeed);
-    std_msgs::msg::Float32 dumpSpeed;
-    dumpSpeed.data = joystick1Yaw;
-    dumpPublisher->publish(dumpSpeed);
-}
-
-/** @brief Function to stop excavation motors
- * 
- * This function is called when the thumb
- * button is pressed and switches to the drive
- * state.  It publishes a speed of 0.0 for
- * the shoulder, excavation arm, drum, and
- * dump bin.
- * @return void
- * */
-void stopExcavation(){
-    std_msgs::msg::Float32 speed;
-    speed.data = 0.0;
-    shoulderPublisher->publish(speed);
-    dumpPublisher->publish(speed);
-    neoPublisher->publish(speed);
-    dumpBinPublisher->publish(speed);
+    talonSpeedPublisher->publish(speed);
+    falconSpeedPublisher->publish(speed);
+    neoSpeedPublisher->publish(speed);
 }
 
 /**
@@ -210,34 +147,29 @@ float transformJoystickInfo(float info, float deadZone){
 void joystickAxisCallback(const messages::msg::AxisState::SharedPtr axisState){
     //RCLCPP_INFO(nodeHandle->get_logger(),"Axis %d %d %f", axisState->joystick, axisState->axis, axisState->state);
     //RCLCPP_INFO(nodeHandle->get_logger(),"Axis %d %f %f %f %f", axisState->joystick, axisState->state0, axisState->state1, axisState->state2, axisState->state3);
-    if(automationGo)
-        automationGo = false;
-    
     float deadZone = 0.1;
     if(axisState->axis==0){
         joystick1Roll = transformJoystickInfo(-axisState->state, deadZone);
-        if(!excavationGo)
-            updateSpeed();
+        updateSpeed();
     }
     else if(axisState->axis==1){
         joystick1Pitch = transformJoystickInfo(axisState->state, deadZone);
-        if(excavationGo)
-            updateExcavation();
-        else
-            updateSpeed();
+        updateSpeed();
     }
     else if(axisState->axis==2){
         joystick1Yaw = transformJoystickInfo(axisState->state, deadZone);
-        if(excavationGo){
-            updateExcavation();
-        }
+        std_msgs::msg::Float32 speed;
+        speed.data  = joystick1Yaw;
+        speed.data  = speed.data * maxSpeed;
+        talonSpeedPublisher->publish(speed);
     }
     else if(axisState->axis==3){
         joystick1Throttle = axisState->state/2 + 0.5;
         joystick1Throttle = transformJoystickInfo(joystick1Throttle, deadZone * maxNeoSpeed);
-        if(excavationGo){
-            updateExcavation();
-        }
+        std_msgs::msg::Float32 speed;
+        speed.data  = joystick1Throttle;
+        speed.data  = speed.data * maxSpeed;
+        neoSpeedPublisher->publish(speed);
     }
 }
 
@@ -257,58 +189,17 @@ void joystickButtonCallback(const messages::msg::ButtonState::SharedPtr buttonSt
     std_msgs::msg::Bool state;
 
     switch (buttonState->button) {
-
         case 0:
             break;
-        case 1: //toggles driving and digging
-            if(buttonState->state){
-                excavationGo = !excavationGo;
-                if(!excavationGo)
-                    stopExcavation();
-                else
-                    stopSpeed();
-            }
-            RCLCPP_INFO(nodeHandle->get_logger(), "Button 2");
+        case 1:
             break;
         case 2:
-            if(buttonState->state){
-                speed.data = 1.0;
-            }
-            else{
-                speed.data = 0.0;
-            }
-            stepperPublisher->publish(speed);
-            RCLCPP_INFO(nodeHandle->get_logger(), "Button 3");
             break;
         case 3:
-            if(buttonState->state){
-                speed.data = -0.25;
-            }
-            else{
-                speed.data = 0.0;
-            }
-            dumpBinPublisher->publish(speed);
-            RCLCPP_INFO(nodeHandle->get_logger(), "Button 4");
             break;
         case 4:
-            if(buttonState->state){
-                speed.data = -1.0;
-            }
-            else{
-                speed.data = 0.0;
-            }
-            stepperPublisher->publish(speed);
-            RCLCPP_INFO(nodeHandle->get_logger(), "Button 5");
             break;
         case 5:
-            if(buttonState->state){
-                speed.data = 0.25;
-            }
-            else{
-                speed.data = 0.0;
-            }
-            dumpBinPublisher->publish(speed);
-            RCLCPP_INFO(nodeHandle->get_logger(), "Button 6");
             break;
         case 6:
             break;
@@ -335,13 +226,6 @@ void joystickButtonCallback(const messages::msg::ButtonState::SharedPtr buttonSt
  * */
 void joystickHatCallback(const messages::msg::HatState::SharedPtr hatState){
     std::cout << "Hat " << (int)hatState->joystick << " " << (int)hatState->hat << " " << (int)hatState->state << std::endl;
-
-    if((int)hatState->state == 1 ){
-    }
-    if((int)hatState->state == 4 ){
-    }
-    if((int)hatState->state == 0 ){
-    }
 }
 
 /** @brief Function to update max speed multiplier
@@ -394,22 +278,8 @@ void keyCallback(const messages::msg::KeyState::SharedPtr keyState){
     std::cout << "Key " << keyState->key << " " << keyState->state << std::endl;
 
     if(keyState->key==115 && keyState->state==1){
-        automationGo= !automationGo;
-        std_msgs::msg::Bool msg;
-        msg.data = automationGo;
-        automationGoPublisher->publish(msg);
-        if(automationGo){
-            automation->setGo();
-        }
-        else{
-            automation->setStop();
-            automation->stopActuators();
-        }
-        RCLCPP_INFO(nodeHandle->get_logger(), "Automation invert.  Current state: %d", automationGo);
     }
     if(keyState->key==120 && keyState->state==1){
-        runSensorlessly = !runSensorlessly;
-        automation->setRunSensorlessly(runSensorlessly);
     }
     if(keyState->key==43 && keyState->state==1){
         updateMaxSpeed(0.1);
@@ -425,80 +295,6 @@ void keyCallback(const messages::msg::KeyState::SharedPtr keyState){
     }
 }
 
-/** @brief Callback function for the zedPosition
- * 
- * This function is caled when the node receives a
- * topic with the name zed_position.  This function
- * extracts the information and calls the setPosition
- * function from the automation problem.  
- * \see .Automation.cpp
- * @param zedPosition \see ZedPosition.msg
- * @return void
- * */
-void zedPositionCallback(const messages::msg::ZedPosition::SharedPtr zedPosition){
-    Position position;
-    position.x=zedPosition->x;	
-    position.y=zedPosition->y;	
-    position.z=zedPosition->z;	
-    position.ox=zedPosition->ox;	
-    position.oy=zedPosition->oy;	
-    position.oz=zedPosition->oz;	
-    position.ow=zedPosition->ow;	
-    position.roll=zedPosition->roll;
-    position.pitch=zedPosition->pitch;
-    position.yaw=zedPosition->yaw;
-    position.aruco_roll=zedPosition->aruco_roll;
-    position.aruco_pitch=zedPosition->aruco_pitch;
-    position.aruco_yaw=zedPosition->aruco_yaw;
-    position.arucoVisible=zedPosition->aruco_visible;	
-    automation->setPosition(position);
-    double yawRadians=automation->orientation.roll;
-
-    double facingUnitX=-sin(yawRadians);
-    double facingUnitZ=cos(yawRadians);
-    double directionX=-5-position.x;
-    double directionZ=2-position.z;
-
-    double theta = acos((facingUnitX*directionX + facingUnitZ*directionZ)/(sqrt(directionX*directionX + directionZ*directionZ)))*180/M_PI;
-    double yaw = yawRadians * 180/M_PI;
-    double deltaYaw = theta-yaw;
-    double yawTolerance=5;
-    std::cout << "roll:" << automation->orientation.roll*180/M_PI << ", pitch:" << automation->orientation.pitch*180/M_PI << ", yaw" << automation->orientation.yaw*180/ M_PI << "   "
-              << "   \tx:" << position.x << " y: " << position.y << " z:" << position.z
-              << "   \tox:" << position.ox << "  oy:" << position.oy << " oz:" << position.oz << "  ow:" << position.ow
-              << "   \tfUX:" << facingUnitX << " fUZ:" << facingUnitZ << "   yaw:" << yaw << " dYaw:" << deltaYaw << " theta:" << theta
-              << "   \tvisible:" << position.arucoVisible << std::endl;
-    RCLCPP_INFO(nodeHandle->get_logger(), "roll: %f, pitch: %f, yaw: %f", automation->orientation.roll*180/M_PI, automation->orientation.pitch*180/M_PI, automation->orientation.yaw*180/ M_PI);
-}
-
-
-/** @brief Callback function for the LinearOut topic
- * 
- * @param linearOut 
- */
-void linearOut1Callback(const messages::msg::LinearOut::SharedPtr linearOut){
-    automation->setLinear1(linearOut);
-}
-
-
-/** @brief Callback function for the LinearOut topic
- * 
- * @param linearOut 
- */
-void linearOut2Callback(const messages::msg::LinearOut::SharedPtr linearOut){
-    automation->setLinear2(linearOut);
-}
-
-
-/** @brief Callback function for the LinearOut topic
- * 
- * @param linearOut 
- */
-void linearOut3Callback(const messages::msg::LinearOut::SharedPtr linearOut){
-    automation->setLinear3(linearOut);
-}
-
-
 int main(int argc, char **argv){
     rclcpp::init(argc,argv);
     nodeHandle = rclcpp::Node::make_shared("logic");
@@ -508,28 +304,15 @@ int main(int argc, char **argv){
     auto joystickButtonSubscriber= nodeHandle->create_subscription<messages::msg::ButtonState>("joystick_button",1,joystickButtonCallback);
     auto joystickHatSubscriber= nodeHandle->create_subscription<messages::msg::HatState>("joystick_hat",1,joystickHatCallback);
     auto keySubscriber= nodeHandle->create_subscription<messages::msg::KeyState>("key",1,keyCallback);
-    auto zedPositionSubscriber= nodeHandle->create_subscription<messages::msg::ZedPosition>("zed_position",1,zedPositionCallback);
-    auto linearOut1Subscriber = nodeHandle->create_subscription<messages::msg::LinearOut>("linearOut1",1,linearOut1Callback);
-    auto linearOut2Subscriber = nodeHandle->create_subscription<messages::msg::LinearOut>("linearOut2",1,linearOut2Callback);
-    auto linearOut3Subscriber = nodeHandle->create_subscription<messages::msg::LinearOut>("linearOut3",1,linearOut3Callback);
 
-    driveLeftSpeedPublisher= nodeHandle->create_publisher<std_msgs::msg::Float32>("drive_left_speed",1);
-    driveRightSpeedPublisher= nodeHandle->create_publisher<std_msgs::msg::Float32>("drive_right_speed",1);
-    shoulderPublisher = nodeHandle->create_publisher<std_msgs::msg::Float32>("shoulder_speed",1);
-    dumpPublisher = nodeHandle->create_publisher<std_msgs::msg::Float32>("dump_speed",1);
-    neoPublisher = nodeHandle->create_publisher<std_msgs::msg::Float32>("neo_speed",1);
-    automationGoPublisher = nodeHandle->create_publisher<std_msgs::msg::Bool>("automationGo",1);
-    dumpBinPublisher = nodeHandle->create_publisher<std_msgs::msg::Float32>("dump_bin_speed",1);
-    stepperPublisher = nodeHandle->create_publisher<std_msgs::msg::Float32>("stepper_speed",1);
+    talonSpeedPublisher= nodeHandle->create_publisher<std_msgs::msg::Float32>("talon_speed",1);
+    falconSpeedPublisher= nodeHandle->create_publisher<std_msgs::msg::Float32>("falcon_speed",1);
+    neoSpeedSpublisher= nodeHandle->create_publisher<std_msgs::msg::Float32>("neo_speed",1);
 
     initSetSpeed();
 
     rclcpp::Rate rate(30);
     while(rclcpp::ok()){
-        if(automationGo){
-            automation->automate();
-            automation->publishAutomationOut();
-        } 
         rclcpp::spin_some(nodeHandle);
         rate.sleep();
     }
